a59984391966e37a6824e4768f56dc77ff7751ab
valnhe <vpazcastle@gmail.com>
2025-05-02 21:54:54 -0400

    create modal incidente

---
+++ frontend/src/pages/supervisor/ModalIncidentes.jsx
@@ -0,0 +1,129 @@
+import { useState } from 'react';
+import tecnicos from "../../mockups/tecnicos.json";
+
+import { Timeline } from 'primereact/timeline';
+import { Dropdown } from 'primereact/dropdown';
+import { formatFecha } from "../../utils/date";
+
+export const ModalIncidentes = ({onClose, incidente}) => {
+
+    const [prioridad, setPrioridad] = useState(incidente.prioridad || null);
+    const [gravedad, setGravedad] = useState(incidente.gravedad || null);
+    const [tecnicoAsignado, setTecnicoAsignado] = useState(incidente.tecnico || null);
+
+    const prioridades = ['Alta', 'Media', 'Baja'];
+    const gravedades = ['Alta', 'Media', 'Baja'];
+
+    const events = [
+        { status: 'Creado', date: formatFecha(incidente.fecha_creado)},
+        { status: 'Técnico Asignado', date: formatFecha(incidente.fecha_tecnico_asignado)},
+        { status: 'En espera de aprobación', date: formatFecha(incidente.fecha_espera_aprovacion)},
+        { status: 'Resuelto', date: formatFecha(incidente.fecha_resuelto)}
+    ];
+
+    const handleGuardar = () => {
+        // Aquí iría tu lógica para guardar los cambios
+        console.log({ prioridad, gravedad, tecnicoAsignado });
+      };
+          
+    return (
+      <div className="modal-overlay">
+        <div className="modal">
+            <section className='info-zone'>
+                <div>
+                {incidente.gravedad && 
+                        <>
+                        <h2>Incidente {incidente.id}</h2>
+                        <h3>Detalles del incidente</h3>
+                        <ul>
+                            <li><b>Lugar: </b><span>{incidente.lugar}</span></li>
+                            <li><b>Descripción: </b><span>{incidente.descripcion}</span></li>
+                            <li><b>Robot involucrado: </b><span>{incidente.robots}</span></li>
+                            <li><b>Prioridad: </b><span>{incidente.prioridad}</span></li>
+                            <li><b>Gravedad: </b><span>{incidente.gravedad}</span></li>
+                            <li><b>Técnico Asignado: </b><span>{incidente.tecnico}</span></li>
+
+                            {incidente.fecha_espera_aprovacion && <li><b>Comentarios sobre el trabajo realizado: </b><span>{incidente.comentario}</span></li>}
+                        </ul>
+                        <div className='button-container'>
+                            <button className='link-button' onClick={handleGuardar}>Editar</button>
+                        </div>
+                        </>
+                    }
+
+                    {!incidente.gravedad &&
+                        <>
+                            <h2>Incidente {incidente.id}</h2>
+                            <h3>Detalles del incidente</h3>
+                            <ul>
+                                <li><b>Lugar:</b> <span>{incidente.lugar}</span></li>
+                                <li><b>Descripción:</b> <span>{incidente.descripcion}</span></li>
+                                <li><b>Robot involucrado: </b><span>{incidente.robots}</span></li>
+                            </ul>
+
+                            <h3>Definir Prioridad</h3>
+                            <Dropdown value={prioridad} 
+                                options={prioridades} 
+                                onChange={(e) => setPrioridad(e.value)} 
+                                placeholder="Selecciona prioridad"
+                                style={{ width: '100%' }}
+                            />
+                            
+                            <h3>Definir Gravedad</h3>
+                            <Dropdown value={gravedad} 
+                                options={gravedades} 
+                                onChange={(e) => setGravedad(e.value)} 
+                                placeholder="Selecciona gravedad"
+                                style={{ width: '100%' }}
+                            />
+
+                            <h3>Asignar Técnico</h3>
+                            <Dropdown 
+                                value={tecnicoAsignado} 
+                                options={tecnicos} 
+                                onChange={(e) => setTecnicoAsignado(e.value)} 
+                                optionLabel="nombre"
+                                placeholder="Selecciona técnico"
+                                style={{ width: '100%' }}
+                            />
+
+                            <div className='button-container'>
+                                <button className='link-button' onClick={handleGuardar}>Guardar</button>
+                            </div>
+                            </>
+                    }
+                </div>
+                <button className="btn-icon" onClick={() => onClose()}>
+                    <svg  xmlns="http://www.w3.org/2000/svg"  width={24}  height={24}  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth={2}  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-x"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M18 6l-12 12" /><path d="M6 6l12 12" /></svg>
+                </button>
+            </section>
+
+            <section className='timeline-zone'>
+            <Timeline
+                value={events}
+                opposite={(item) => item.status}
+                marker={(item) => (
+                    <span
+                    style={{
+                        backgroundColor: item.date ? '#5C90C5' : '#B0B0B0', // azul o gris
+                        width: '1rem',
+                        height: '1rem',
+                        display: 'inline-block',
+                        borderRadius: '50%',
+                    }}
+                    />
+                )}
+                content={(item) => (
+                    <small style={{ color: item.date ? 'inherit' : '#B0B0B0' }}>
+                    {item.date ?? ''}
+                    </small>
+                )}
+                />
+            </section>
+
+        </div>
+      </div>
+    )
+  }
+  
+  export default ModalIncidentes;
