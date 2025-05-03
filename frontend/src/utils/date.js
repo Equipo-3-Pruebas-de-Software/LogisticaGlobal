862ed2186c0d6b2ff6fb69a982746e8598b67b31
valnhe <vpazcastle@gmail.com>
2025-05-02 21:54:05 -0400

    update date function

--- frontend/src/utils/date.js
+++ frontend/src/utils/date.js
@@ -1,9 +1,12 @@
 export const formatFecha = (fechaISO) => {
+
+    if (!fechaISO) return null;
+    
     const fecha = new Date(fechaISO);
     const dia = String(fecha.getDate()).padStart(2, '0');
     const mes = String(fecha.getMonth() + 1).padStart(2, '0'); // Los meses empiezan en 0
     const anio = fecha.getFullYear();
     const horas = String(fecha.getHours()).padStart(2, '0');
     const minutos = String(fecha.getMinutes()).padStart(2, '0');
-    return `${dia}-${mes}-${anio} ${horas}:${minutos}`;
+    return `${dia}/${mes}/${anio} ${horas}:${minutos}`;
   };
