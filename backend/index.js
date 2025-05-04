217ca269b25ac9455ce77b3bd8bec5af9cb33ba4
valnhe <vpazcastle@gmail.com>
2025-05-04 17:46:40 -0400

    fixing bug

--- backend/index.js
+++ backend/index.js
@@ -36,4 +36,5 @@ app.use('/api/auth', authRoutes);
 // Iniciar servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
+});
