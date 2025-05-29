4c53bf68650137b864f65e44ee379bee762ced38
Hakdyr <eljoaquino@hotmail.com>
2025-05-29 22:45:04 +0000

    Cambio de IP

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
         e.preventDefault();
 
         try {
-            const response = await fetch('http://3.142.70.13:3000/api/auth/login', {
+            const response = await fetch('http://18.225.35.240:3000/api/auth/login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ rut, clave }),
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,32 +8,32 @@ export default defineConfig({
     proxy: {
       // Todas las peticiones a /incidentes se enviarán a tu backend en el 3000
       '/incidentes': {
-        target: 'http://3.142.70.13/:3000',
+        target: 'http://18.225.35.240:3000',
         changeOrigin: true,
         // rewrite no hace falta si la ruta es idéntica
       },
       '/tecnicos': {
-        target: 'http://3.142.70.13:3000',
+        target: 'http://18.225.35.240:3000',
         changeOrigin: true,
         // rewrite no hace falta si la ruta es idéntica
       },
       '/robots': {
-        target: 'http://3.142.70.13:3000',
+        target: 'http://18.225.35.240:3000',
         changeOrigin: true,
         // rewrite no hace falta si la ruta es idéntica
       },
       '/incidentes-robots-tecnicos/asignar-tecnico': {
-        target: 'http://3.142.70.13:3000',
+        target: 'http://18.225.35.240:3000',
         changeOrigin: true,
         // rewrite no hace falta si la ruta es idéntica
       },
       '/incidentes-robots-tecnicos': {
-        target: 'http://3.142.70.13:3000',
+        target: 'http://18.225.35.240:3000',
         changeOrigin: true,
         // rewrite no hace falta si la ruta es idéntica
       },
       '/api/auth': {
-        target: 'http://3.142.70.13:3000',
+        target: 'http://18.225.35.240:3000',
         changeOrigin: true,
         // rewrite no hace falta si la ruta es idéntica
       }
