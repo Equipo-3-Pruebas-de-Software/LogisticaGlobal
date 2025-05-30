5dcc0dcbea9f53898654e063bf2df7b2b7e57161
Hakdyr <eljoaquino@hotmail.com>
2025-05-30 00:26:08 +0000

    cambio de IP

--- frontend/src/pages/tecnico/robots_asignados.jsx
+++ frontend/src/pages/tecnico/robots_asignados.jsx
@@ -14,7 +14,7 @@ const RobotsAsignados = () => {
 
     const fetchRobots = async () => {
       try {
-        const response = await fetch(`http://18.225.35.240:3000/tecnicos/robots-asignados/${usuario.rut}`);
+        const response = await fetch(`http://18.217.42.134:3000/tecnicos/robots-asignados/${usuario.rut}`);
         const data = await response.json();
         setRobots(data);
       } catch (err) {
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,32 +8,32 @@ export default defineConfig({
     proxy: {
       // Todas las peticiones a /incidentes se enviarán a tu backend en el 3000
       '/incidentes': {
-        target: 'http://18.225.35.240:3000',
+        target: 'http://18.217.42.134:3000',
         changeOrigin: true,
         // rewrite no hace falta si la ruta es idéntica
       },
       '/tecnicos': {
-        target: 'http://18.225.35.240:3000',
+        target: 'http://18.217.42.134:3000',
         changeOrigin: true,
         // rewrite no hace falta si la ruta es idéntica
       },
       '/robots': {
-        target: 'http://18.225.35.240:3000',
+        target: 'http://18.217.42.134:3000',
         changeOrigin: true,
         // rewrite no hace falta si la ruta es idéntica
       },
       '/incidentes-robots-tecnicos/asignar-tecnico': {
-        target: 'http://18.225.35.240:3000',
+        target: 'http://18.217.42.134:3000',
         changeOrigin: true,
         // rewrite no hace falta si la ruta es idéntica
       },
       '/incidentes-robots-tecnicos': {
-        target: 'http://18.225.35.240:3000',
+        target: 'http://18.217.42.134:3000',
         changeOrigin: true,
         // rewrite no hace falta si la ruta es idéntica
       },
       '/api/auth': {
-        target: 'http://18.225.35.240:3000',
+        target: 'http://18.217.42.134:3000',
         changeOrigin: true,
         // rewrite no hace falta si la ruta es idéntica
       }
