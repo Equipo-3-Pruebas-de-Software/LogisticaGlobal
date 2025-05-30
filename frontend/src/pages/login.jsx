118f929d732516957fe657b4a7d38adc4082269a
Hakdyr <eljoaquino@hotmail.com>
2025-05-30 16:01:58 +0000

    Actualizacion IP y config jenkins

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
         e.preventDefault();
 
         try {
-            const response = await fetch('http://3.129.17.189:3000/api/auth/login', {
+            const response = await fetch('http://18.116.163.236:3000/api/auth/login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ rut, clave }),
