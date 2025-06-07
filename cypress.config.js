7e4024ad915b4522199aefcdb2e92beadf68023d
Hakdyr <eljoaquino@hotmail.com>
2025-06-07 03:36:55 -0400

    Actualizacion archivo cypress.config

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,15 @@
-const { defineConfig } = require("cypress");
-
-module.exports = defineConfig({
+module.exports = {
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: "http://localhost:3000", // Cambia esto a la URL base de tu aplicación
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
   },
-});
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "webpack",
+    },
+  },
+};
