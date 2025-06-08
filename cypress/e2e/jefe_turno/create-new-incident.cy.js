8912eeb9e4d4621cf2ac4ab8ef43c1d8969cc556
Hakdyr <eljoaquino@hotmail.com>
2025-06-08 16:56:54 -0400

    cambio 3 cni

--- cypress/e2e/jefe_turno/create-new-incident.cy.js
+++ cypress/e2e/jefe_turno/create-new-incident.cy.js
@@ -1,29 +1,44 @@
 describe('Crear Incidentes', () => {
   beforeEach(() => {
     cy.visit('http://192.168.56.1:5173/');
-    cy.get('input[id="rut"]', { timeout: 10000 }).should('be.visible').type('11111111-1');
-    cy.get('input[id="password"]').type('clave123');
-    cy.get('button[type="submit"]').click();
-    cy.url().should('include', '/jefe_turno');
-    // Esperar a que la página esté completamente cargada
-    cy.get('h1').contains('Panel Jefe de Turno').should('be.visible');
+    
+    // Verificar que la página de login está cargada
+    cy.get('input[id="rut"]', { timeout: 15000 })
+      .should('be.visible')
+      .type('11111111-1');
+      
+    cy.get('input[id="password"]')
+      .should('be.visible')
+      .type('clave123');
+      
+    cy.get('button[type="submit"]')
+      .should('be.visible')
+      .click();
+    
+    // Verificar redirección después del login
+    cy.url({ timeout: 15000 })
+      .should('include', '/jefe_turno');
+    
+    // Verificar que algún elemento único de la página de jefe de turno está visible
+    cy.get('div.jefe-turno-container', { timeout: 15000 })
+      .should('be.visible');
   });
 
   const cerrarSesion = () => {
-    cy.get('a').contains('Cerrar sesión').click(); 
-    cy.url().should('include', '/'); 
-    cy.get('input[id="rut"]').should('be.visible'); 
+    cy.get('a').contains('Cerrar sesión')
+      .should('be.visible')
+      .click();
+    cy.url().should('include', '/');
+    cy.get('input[id="rut"]').should('be.visible');
   };
 
   const abrirMultiSelect = () => {
-    // Esperar y verificar que el multiselect esté visible y habilitado
     cy.get('div[id="robots"]', { timeout: 15000 })
       .should('be.visible')
       .and('not.be.disabled')
       .click();
     
-    // Esperar a que el dropdown esté completamente abierto
-    cy.get('div.p-multiselect-panel', { timeout: 10000 })
+    cy.get('div.p-multiselect-panel', { timeout: 15000 })
       .should('be.visible')
       .find('li.p-multiselect-item')
       .should('have.length.gt', 0);
@@ -32,19 +47,16 @@ describe('Crear Incidentes', () => {
   const seleccionarRobots = () => {
     abrirMultiSelect();
     
-    // Seleccionar el primer robot
     cy.get('li.p-multiselect-item')
       .first()
       .should('be.visible')
       .click();
     
-    // Seleccionar el último robot
     cy.get('li.p-multiselect-item')
       .last()
       .should('be.visible')
       .click();
 
-    // Cerrar el dropdown
     cy.get('body').click(0, 0);
   };
 
@@ -55,7 +67,7 @@ describe('Crear Incidentes', () => {
     seleccionarRobots();
 
     cy.get('button[type="submit"]').click();
-    cy.get('.p-toast-message-success', { timeout: 15000 }) 
+    cy.get('.p-toast-message-success', { timeout: 20000 })
       .should('be.visible')
       .and('contain', 'Incidente creado');
 
@@ -68,7 +80,7 @@ describe('Crear Incidentes', () => {
     seleccionarRobots();
 
     cy.get('button[type="submit"]').click();
-    cy.get('.p-toast-message-error', { timeout: 15000 }) 
+    cy.get('.p-toast-message-error', { timeout: 20000 })
       .should('be.visible')
       .and('contain', 'Todos los campos son obligatorios');
 
@@ -81,7 +93,7 @@ describe('Crear Incidentes', () => {
     seleccionarRobots();
 
     cy.get('button[type="submit"]').click();
-    cy.get('.p-toast-message-error', { timeout: 15000 }) 
+    cy.get('.p-toast-message-error', { timeout: 20000 })
       .should('be.visible')
       .and('contain', 'Todos los campos son obligatorios');
       
@@ -93,7 +105,7 @@ describe('Crear Incidentes', () => {
     cy.get('textarea[id="descripcion"]').type("El robot se detuvo repentinamente en la zona de carga, causando un retraso en la operación. Se recomienda revisar el estado del robot y reiniciar su sistema.");
     
     cy.get('button[type="submit"]').click();
-    cy.get('.p-toast-message-error', { timeout: 15000 }) 
+    cy.get('.p-toast-message-error', { timeout: 20000 })
       .should('be.visible')
       .and('contain', 'Todos los campos son obligatorios');
 
