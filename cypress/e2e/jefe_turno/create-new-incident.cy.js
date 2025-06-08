ae08ccb6b5646392136287806859982445b80097
Hakdyr <eljoaquino@hotmail.com>
2025-06-08 16:31:34 -0400

    volver inicio create new incident

--- cypress/e2e/jefe_turno/create-new-incident.cy.js
+++ cypress/e2e/jefe_turno/create-new-incident.cy.js
@@ -1,41 +1,34 @@
 describe('Crear Incidentes', () => {
   beforeEach(() => {
     cy.visit('http://192.168.56.1:5173/');
-    cy.get('input[id="rut"]', { timeout: 30000 }).type('11111111-1');
-    cy.get('input[id="password"]', { timeout: 30000 }).type('clave123');
-    cy.get('button[type="submit"]', { timeout: 30000 }).click();
-    cy.url({ timeout: 30000 }).should('include', '/jefe_turno');
+    cy.get('input[id="rut"]').type('11111111-1');
+    cy.get('input[id="password"]').type('clave123');
+    cy.get('button[type="submit"]').click();
+    cy.url().should('include', '/jefe_turno');
   });
 
   const cerrarSesion = () => {
-    cy.get('a').contains('Cerrar sesión', { timeout: 15000 }).click(); 
+    cy.get('a').contains('Cerrar sesión').click(); 
     cy.url().should('include', '/'); 
     cy.get('input[id="rut"]').should('be.visible'); 
   };
 
-  const seleccionarRobots = () => {
-    // Hacer clic y esperar a que el dropdown esté visible
-    cy.get('div[id="robots"]', { timeout: 30000 }).click();
-    cy.get('div.p-multiselect-panel', { timeout: 30000 })
-      .should('be.visible')
-      .find('li.p-multiselect-item', { timeout: 30000 })
-      .should('have.length.gt', 0)
-      .first()
-      .click();
-    
-    // Cerrar el dropdown haciendo clic en el cuerpo
-    cy.get('body').click(0, 0);
-  };
-
   it('debería poder crear un nuevo incidente', () => {
-    cy.get('input[id="lugar"]', { timeout: 30000 }).type('Pasillo 3');
-    cy.get('textarea[id="descripcion"]', { timeout: 30000 })
-      .type('Los robots chocaron. A uno de ellos se le salió la rueda izquierda, al otro se le rompió el brazo mecánico. Dejaron caer mercancías frágiles.');
-    
-    seleccionarRobots();
+    cy.get('input[id="lugar"]').type('Pasillo 3');
+    cy.get('textarea[id="descripcion"]').type('Los robots chocaron. A uno de ellos se le salió la rueda izquierda, al otro se le rompió el brazo mecánico. Dejaron caer mercancías frágiles.');
+    cy.get('div[id="robots"]').click();
+    cy.get('li.p-multiselect-item')
+      .first()
+      .click(); 
 
-    cy.get('button[type="submit"]', { timeout: 30000 }).click();
-    cy.get('.msg', { timeout: 30000 }) 
+    cy.get('li.p-multiselect-item')
+      .last()
+      .click(); 
+
+    cy.get('body').click(0, 0);
+
+    cy.get('button[type="submit"]').click();
+    cy.get('.msg') 
       .should('be.visible')
       .and('contain', 'Incidente creado');
 
@@ -43,13 +36,17 @@ describe('Crear Incidentes', () => {
   });
 
   it('debería dar error porque falta el campo de lugar', () => {
-    cy.get('textarea[id="descripcion"]', { timeout: 30000 })
-      .type("Durante la manipulación de paquetes frágiles, el robot dejó caer una caja, resultando en daños en el contenido. Revisar sensores.");
-    
-    seleccionarRobots();
+    cy.get('textarea[id="descripcion"]').type("Durante la manipulación de paquetes frágiles, el robot dejó caer una caja, resultando en daños en el contenido. Revisar sensores.");
+    cy.get('div[id="robots"]').click();
+  
+    cy.get('li.p-multiselect-item')
+      .first()
+      .click();
 
-    cy.get('button[type="submit"]', { timeout: 30000 }).click();
-    cy.get('.msg', { timeout: 30000 }) 
+    cy.get('body').click(0, 0);
+
+    cy.get('button[type="submit"]').click();
+    cy.get('.msg') 
       .should('be.visible')
       .and('contain', 'Todos los campos son obligatorios');
 
@@ -57,28 +54,33 @@ describe('Crear Incidentes', () => {
   });
 
   it('debería dar error porque falta el campo de descripción', () => {
-    cy.get('input[id="lugar"]', { timeout: 30000 }).type('Pasillo Norte');
-    
-    seleccionarRobots();
+    cy.get('input[id="lugar"]').type('Pasillo Norte');
+    cy.get('div[id="robots"]').click();
 
-    cy.get('button[type="submit"]', { timeout: 30000 }).click();
-    cy.get('.msg', { timeout: 30000 }) 
+    cy.get('li.p-multiselect-item')
+      .first()
+      .click(); 
+
+    cy.get('body').click(0, 0);
+
+    cy.get('button[type="submit"]').click();
+    cy.get('.msg') 
       .should('be.visible')
       .and('contain', 'Todos los campos son obligatorios');
-    
-    cerrarSesion();
   });
 
   it('debería dar error porque no se seleccionaron robots', () => {
-    cy.get('input[id="lugar"]', { timeout: 30000 }).type('Pasillo Este');
-    cy.get('textarea[id="descripcion"]', { timeout: 30000 })
-      .type("El robot se detuvo repentinamente en la zona de carga, causando un retraso en la operación. Se recomienda revisar el estado del robot y reiniciar su sistema.");
+    cy.get('input[id="lugar"]').type('Pasillo Este');
+    cy.get('textarea[id="descripcion"]').type("El robot se detuvo repentinamente en la zona de carga, causando un retraso en la operación. Se recomienda revisar el estado del robot y reiniciar su sistema.");
     
-    cy.get('button[type="submit"]', { timeout: 30000 }).click();
-    cy.get('.msg', { timeout: 30000 }) 
+    cy.get('body').click(0, 0);
+    
+    cy.get('button[type="submit"]').click();
+    cy.get('.msg') 
       .should('be.visible')
       .and('contain', 'Todos los campos son obligatorios');
 
     cerrarSesion();
   });
+
 });
