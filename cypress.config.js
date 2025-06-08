fca6b419eabfd1f8dd55edf15ee60ed2033c793b
Hakdyr <eljoaquino@hotmail.com>
2025-06-08 12:11:10 -0400

    Prueba

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ module.exports = {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: "http://localhost:3000", // Cambia esto a la URL base de tu aplicación
+    baseUrl: "http://192.168.56.1:3000", // Cambia esto a la URL base de tu aplicación
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
   },
   component: {
